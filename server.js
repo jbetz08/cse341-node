63b3bd55c1bed3ab08e91bfb1f1e61bcd194305e
Josh Betzsold <josh.betzsold@amenity.health>
2022-09-17 20:19:33 -0600

    basics changes

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.use('/', require('./routes'))
 
-app.listen(port, () => {
+app.listen(3000, function() {
   console.log(`Example app listening on port ${port}`)
 })
